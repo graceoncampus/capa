b314d46ca96579d9ae62d5473815a99208775b4b
Ryan Voong <ryanvoong@gmail.com>
2015-10-31 13:51:57 -0700

    Refactor csv parser/loader

--- bin/parse.js
+++ bin/parse.js
@@ -1,28 +1,26 @@
+var fs = require('fs');
 var parse = require('csv-parse');
 var Student = require('../models/student');
 
-var fs = require('fs');
-var array = fs.readFileSync('test.csv').toString();
-parse(array, function (err, output) {
-    output.forEach( function (student) {
-            var s = new Student ({
-                name: student[0],
-                program: student[1],
-                age: student[2],
-                hometown: student[3],
-                family: student[4],
-                quote: student[5],
-                summary: student[6]
-            });
-            s.interview = [];
-            var subarray = student.slice(7);
-            for (var i = 0; i < subarray.length - 1; i += 2) {
-                s.interview.append({question: subarray[i], answer: subarray[i+1]});
-            }
-            s.save();
-            console.log(s);
+var data = fs.readFileSync('test.csv').toString();
+parse(data, function (err, output) {
+    output.forEach(function (student) {
+        var s = new Student ({
+            name: student[0],
+            program: student[1],
+            age: student[2],
+            hometown: student[3],
+            family: student[4],
+            quote: student[5],
+            summary: student[6]
+        });
+        s.interview = [];
+        var subarray = student.slice(7);
+        for (var i = 0; i < subarray.length - 1; i += 2) {
+            s.interview.append({question: subarray[i], answer: subarray[i+1]});
         }
-    )
+        console.log(s);
+        s.save();
+    });
+    console.log('Finished loading the students!');
 });
-
-
