aeb738c19a35eeb0253745aff6f1b603b1a8d1c5
Jennifer Tan <jennifertan213@gmail.com>
2015-11-08 16:39:52 -0800

    Added button filters to display students by program.

--- bin/seed.js
+++ bin/seed.js
@@ -2,10 +2,33 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/capadb');
 var Student = require('../models/student');
 
-for (var i = 0; i < 20; i++) {
+for (var i = 0; i < 2; i++) {
   var s = new Student({
-    name: 'Chris Gee',
-    program: 0, // 0 is Advanced Diploma, 1 is MDiv
+    name: 'Chris mdi',
+    program: 'mdiv', // 0 is Advanced Diploma, 1 is MDiv
+    age: 30,
+    hometown: 'Oakland, CA',
+    family: 'Wife and 2 kids',
+    quote: 'im supah bent',
+    summary: 'UCLA GOC shepherd',
+    photoURI: '/images/chris.jpg',
+    interview: [
+      {
+        question: 'who r u',
+        answer: 'i am the chris'
+      }, {
+        question: 'why chris',
+        answer: 'idk'
+      }
+    ]
+  });
+  console.log(s);
+  s.save();
+}
+for (var i = 0; i < 2; i++) {
+  var s = new Student({
+    name: 'Chris dip',
+    program: 'diploma', // 0 is Advanced Diploma, 1 is MDiv
     age: 30,
     hometown: 'Oakland, CA',
     family: 'Wife and 2 kids',
---
+++ views/application.js
@@ -0,0 +1,14 @@
+//$(document).ready(function() {
+//    $('.diploma').on('click', function() {
+//        // run this function on click
+//        $('.highlighted').removeClass('.highlighted');
+//        $('.students').filter('.diploma').addClass('.highlighted');
+//    });
+//    $('.masters').on('click', function() {
+//        $('.highlighted').removeClass('.highlighted');
+//        $('.students').filter('.diploma').addClass('.highlighted');
+//    });
+//    $('.show-all').on('click', function() {
+//        $('.highlighted').removeClass('.highlighted');
+//    });
+//});
