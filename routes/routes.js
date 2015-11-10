eb49d9315b5652563b7b46e4c7016631187d7b81
Janet Lee <janetlee379@g.ucla.edu>
2015-11-09 17:39:50 -0800

    Added brand Profile page info Support button link

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ var Student = require('../models/student');
 module.exports.getHome = function(req, res) {
   Student.find({}, function(err, students) {
     res.render('index.ejs', {
-      title: 'CAPA',
+      title: 'home',
       students: students
     });
   });
@@ -12,9 +12,9 @@ module.exports.getHome = function(req, res) {
 
 /* GET profile page. */
 module.exports.getProfile = function(req, res) {
-  Student.find({ _id: req.params.sid }, function(err, student) {
+  Student.findOne({ _id: req.params.sid }, function(err, student) {
     res.render('profile', {
-      title: 'Profile',
+      title: 'profile',
       student: student
     });
   });
