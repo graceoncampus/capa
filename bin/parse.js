c54c72b64d837433dbe8f614eb6989da911fe66d
Ryan Voong <ryanvoong@gmail.com>
2016-12-07 18:27:12 -0800

    update student status

--- bin/parse.js
+++ bin/parse.js
@@ -8,19 +8,24 @@ var data = fs.readFileSync('students.csv').toString();
 
 parse(data, function (err, output) {
     output.forEach(function (student) {
+        if (student[5] && student[5] === "Hide") {
+            return;
+        }
+
         var s = new Student ({
             name: student[0],
             photoURI: student[3],
             program: student[4],
-            age: student[5],
-            location: student[6],
-            family: student[7],
-            quote: student[8],
-            summary: student[9]
+            status: student[5],
+            age: student[6],
+            location: student[7],
+            family: student[8],
+            quote: student[9],
+            summary: student[10]
         });
 
         s.audio = [];
-        var audio = student.slice(10, 12);
+        var audio = student.slice(11, 13);
         audio.forEach(function(URI) {
             if (URI != "") {
                 s.audio.push(URI);
@@ -28,7 +33,7 @@ parse(data, function (err, output) {
         });
 
         s.audioTaglines = [];
-        var audioTaglines = student.slice(12, 14);
+        var audioTaglines = student.slice(13, 15);
         audioTaglines.forEach(function(tagline) {
             if (tagline != "") {
                 s.audioTaglines.push(tagline);
@@ -36,7 +41,7 @@ parse(data, function (err, output) {
         });
 
         s.interview = [];
-        var interview = student.slice(14);
+        var interview = student.slice(15);
         for (var i = 1; i < interview.length; i += 2) {
             if (interview[i-1] != "" && interview[i] != "") {
                 s.interview.push({
--- models/student.js
+++ models/student.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var studentSchema = mongoose.Schema({
     name: String,
     program: String, // diploma, mdiv
+    status: String, // current, graduate, hide
     age: Number,
     location: String,
     family: String,
